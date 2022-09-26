1d497f23f5c1fa8bebc0b4b17545ceabd601915b
gabrielfndd <104361681+gabrielfndd@users.noreply.github.com>
2022-09-26 12:06:25 -0300

    Add files via upload

---
+++ conecxao/conexao/src/connect/conexaobd.java
@@ -0,0 +1,20 @@
+package connect;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import java.sql.DriverManager;
+public class conexaobd {
+    public Connection Connection(){ 
+        Connection con = null;
+        try {
+            String url = "jdbc:mysql//localhost:3306//crudmk?user=root&password=";
+            con = DriverManager.getConnection(url);
+        }catch (Exception erro){
+            JOptionPane.showMessageDialog(null, "Connection"+ erro.getMessage());
+        }
+        return con;
+    }
+            
+    
+}
